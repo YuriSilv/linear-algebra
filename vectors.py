9d74d1065c4ceb24c741745d0261a412130aa4ea
Yuri <yuridtna2002@hotmail.com>
2023-02-24 17:15:54 -0300

    base fucs

--- vectors.py
+++ vectors.py
@@ -1,4 +1,5 @@
 from typing import TypeVar, List
+import math
 
 vector = List[float]
 
@@ -12,7 +13,27 @@ def vec_sum(x:vector, y:vector) -> vector:
 def vec_scale(x:vector, scale:float) -> vector:
     return [value*scale for value in x]
 
+def vec_norm(x: vector) -> float:
+    return math.sqrt(sum([value**2 for value in x]))
+
+def vec_unit(x: vector) -> vector:
+    return vec_scale(x, (1/vec_norm(x)))
+
+def ang_vec2(ang:float, mag:float) -> vector:
+    x = round(math.cos(ang)*mag, 3)
+    y = round(math.sin(ang)*mag, 3)
+    return [x,y]
+
+def linear_combination(v:List[vector], c:list) -> vector:
+    n_vecs = [vec_scale(vetor,c_value) for vetor, c_value in zip(v,c)]
+    result = [0]*len(v[0])
+    for v in n_vecs:
+        result = vec_sum(result, v)
+
+    return result
+
+
 v1 = [1,2,3]
 v2 = [1,1,1]
 
-print(vec_sum(v1,vec_scale(v2,-1)))
+print(linear_combination([[1,2],[0,3]], [2,-2/3]))
